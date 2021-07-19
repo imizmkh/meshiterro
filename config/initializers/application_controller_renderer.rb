a5c75688d5aa5807819b788408fcff8a731f10e8
imizmkh <k.imaizumi1122@gmail.com>
2021-07-19 13:05:17 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f354987f94cc9b7507d119d9210246e5d2f43059398ce7c96c86bd01ae70a3b48e55e387aeb30a5b248ef61d99cdaf4ff638fc199d98cbe54b4fd858a541734d'
